cfd0328b77d39af299cabd3b787c147fdd09bec2
gustavovinireis <gustavovinireis@gmail.com>
2025-09-24 21:09:46 +0000

    adcionado funcao scanf estado

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das cartas
-// Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
-char estado[2];
-char cidade[20];
-int habitantes;
-float area;
-float pib;
-int turismo;
+char estado = 'B';
+char codigo[3] = 'A01';
+char cidade[20] = 'cubatao';
+int habitantes = 112.476;
+float area = 142.9;
+float pib = 21.946;
+int turismo = 7;
   // Área para entrada de dados
-scanf;
-printf;
-
+printf("Digite a letra do estado: \n");
+    scanf("%d", &estado);
+    printf("O estado é: %d\n", estado);
   // Área para exibição dos dados da cidade
-printf ("Estado:%s\n", estado);
+printf ("Estado:%d\n", estado);
+printf ("Código da Carta:%s\n", codigo);
+//Código da Carta: A letra do estado seguida de um número de 01 a 04 (ex: A01, B03). Tipo: char[] (um array de caracteres, ou string)
 printf ("cidade:%s\n", cidade);
 printf ("população:%d\n", habitantes);
 printf ("Área:%.2f km²\n", area);
