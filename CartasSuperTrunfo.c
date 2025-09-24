e12d4169ddc75d94d10a3cc8798dbecd0180a28f
gustavovinireis <gustavovinireis@gmail.com>
2025-09-24 19:13:45 +0000

    inseri as variaveis e a forma que serao impressas

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,10 +6,22 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
-
+char estado[2];
+char cidade[20];
+int habitantes;
+float area;
+float pib;
+int turismo;
   // Área para entrada de dados
+scanf;
+printf;
 
   // Área para exibição dos dados da cidade
-
+printf ("Estado:%s\n", estado);
+printf ("cidade:%s\n", cidade);
+printf ("população:%d\n", habitantes);
+printf ("Área:%.2f km²\n", area);
+printf ("PIB: %.2f bilhões de reais\n", pib);
+printf ("Número de Pontos Turísticos: %d\n", turismo);
 return 0;
 } 
