ae8e939fbbfc26d557643a6a741ee4ce39ad46be
gustavovinireis <gustavovinireis@gmail.com>
2025-09-25 04:56:07 +0000

    inserido mais solicitaçoes de dados para o usuario e corrigido o erro de variavel

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -3,25 +3,45 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
-char estado = 'B';
-char codigo[3] = 'A01';
-char cidade[20] = 'cubatao';
-int habitantes = 112.476;
+char estado;
+char codigo[3];
+char cidade[20] = "cubatao";
+int habitantes = 112476;
 float area = 142.9;
 float pib = 21.946;
 int turismo = 7;
   // Área para entrada de dados
+  printf("Bem vindo ao  jogo Super Trunfo de Países e Cidades!\n\n");
+  Printf("Vamos cadastrar a carta 1:\n");
 printf("Digite a letra do estado: \n");
-    scanf("%d", &estado);
-    printf("O estado é: %d\n", estado);
+    scanf("%c", &estado);
+    printf("O estado é: %c\n", estado);
+    printf("Digite a letra do estado seguida de um número de 01 a 04:\n");
+    scanf("%s", codigo);
+    printf("O codigo da carta é: %s\n", codigo);
+    printf("Digite o nome da cidade:\n");
+    scanf("%s", cidade);
+    printf("A cidade é: %s\n", cidade);
+    printf("Digite o número de habitantes:\n");
+    scanf("%d", &habitantes);
+    printf("A população é: %i\n", habitantes);
+    printf("Digite a área em km²:\n");
+    scanf("%f", &area);
+    printf("A área é: %.2f km²\n", area);
+    printf("Digite o PIB em bilhões de reais:\n");
+    scanf("%f", &pib);
+    printf("O PIB é: %.2f bilhões de reais\n", pib);
+    printf("Digite o número de pontos turísticos:\n");
+    scanf("%d", &turismo);
+    printf("O número de pontos turísticos é: %d\n", turismo);
   // Área para exibição dos dados da cidade
-printf ("Estado:%d\n", estado);
-printf ("Código da Carta:%s\n", codigo);
-//Código da Carta: A letra do estado seguida de um número de 01 a 04 (ex: A01, B03). Tipo: char[] (um array de caracteres, ou string)
+printf("carta 1 \n\n");
+  printf ("Estado:%c\n", estado);
+printf ("Código da Carta:%3s\n", codigo);
 printf ("cidade:%s\n", cidade);
-printf ("população:%d\n", habitantes);
+printf ("população:%i\n", habitantes);
 printf ("Área:%.2f km²\n", area);
 printf ("PIB: %.2f bilhões de reais\n", pib);
-printf ("Número de Pontos Turísticos: %d\n", turismo);
+printf ("Número de Pontos Turísticos: %d\n\n\n", turismo);
 return 0;
 } 
