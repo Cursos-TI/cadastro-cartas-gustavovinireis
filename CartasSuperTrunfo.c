6de4ed7d5f824444c7671f45a128b5decaab1bfb
gustavovinireis <gustavovinireis@gmail.com>
2025-09-25 05:12:29 +0000

    inserido os comandos para a carta 2

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -44,5 +44,28 @@ printf ("população:%i\n", habitantes);
 printf ("Área:%.2f km²\n", area);
 printf ("PIB: %.2f bilhões de reais\n", pib);
 printf ("Número de Pontos Turísticos: %d\n\n\n", turismo);
+
+  printf("Vamos cadastrar a carta 2! \n\n declare as informações solicitadas abaixo: \n\n");
+printf("Digite a letra do estado: \n");
+    scanf(" %c", &estado);
+    printf("O estado é: %c\n", estado);
+    printf("Digite a letra do estado seguida de um número de 01 a 04:\n");
+    scanf("%s", codigo);
+    printf("O codigo da carta é: %s\n", codigo);
+    printf("Digite o nome da cidade:\n");
+    scanf("%s", cidade);
+    printf("A cidade é: %s\n", cidade);
+    printf("Digite o número de habitantes:\n");
+    scanf("%d", &habitantes);
+    printf("A população é: %i\n", habitantes);
+    printf("Digite a área em km²:\n");
+    scanf("%f", &area);
+    printf("A área é: %.2f km²\n", area);
+    printf("Digite o PIB em bilhões de reais:\n");
+    scanf("%f", &pib);
+    printf("O PIB é: %.2f bilhões de reais\n", pib);
+    printf("Digite o número de pontos turísticos:\n");
+    scanf("%d", &turismo);
+    printf("O número de pontos turísticos é: %d\n", turismo);
 return 0;
 } 
